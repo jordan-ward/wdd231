2a8e90749ef0e2913622ea3ed9f38f386ae5ea31
jordan-ward <fallbrook43@gmail.com>
2025-03-27 21:55:48 -0600

    Weather

---
+++ chamber/scripts/index.js
@@ -0,0 +1,63 @@
+// ---------- NAV ----------
+
+// Hamburger Menu
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu')
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show');
+});
+// ---------- MAIN ----------
+
+// Weather API
+const myCity = document.querySelector('#city');
+const myDescription = document.querySelector('#description');
+const myTemperature = document.querySelector('#temperature');
+const myGraphic = document.querySelector('#graphic');
+
+const myKey = "945d3fada872cdbca03d46101f954374"
+const myLat = "43.6150"
+const myLong = "116.2023"
+
+const myURL = `//api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`
+
+async function apiFetch() {
+    try {
+      const response = await fetch(myURL);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // testing only
+        // displayResults(data); // uncomment when ready
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+function displayResults(data) {
+    console.log('Hello')
+    myCity.innerHTML = data.name
+    myDescription.innerHTML = data.weather[0].description
+    myTemperature.innerHTML = `${data.main.temp}&deg;F`
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0]}@2x.png`
+    myGraphic.setAttribute('SRC', iconsrc)
+    myGraphic.setAttribute('alt', data.weather[0].description)
+}
+
+// ---------- FOOTER ----------
+
+// Get Date / Last Modified
+const currentyear = document.querySelector("#currentyear");
+const lastmodified = document.querySelector("#lastmodified");
+
+const today = new Date();
+
+currentyear.innerHTML = `<span>${today.getFullYear()}</span>`;
+
+const date = new Date(document.lastModified);
+document.getElementById("lastmodified").innerHTML = date;
